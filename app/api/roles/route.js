8f77d1a876206465449998fe9870c214f14c7b1e
ghaith alqattan <1vvsqm@gmail.com>
2025-04-08 16:39:20 +0300

    roles route last update

--- app/api/roles/route.js
+++ app/api/roles/route.js
@@ -4,21 +4,36 @@ import { ZodError } from 'zod'
 import verify from '@/utils/validate_token'
 import connectMongoDB from '@/utils/mongodb'
 
-export default async function POST(req, res) {
+export async function POST(req) {
+  const data = await req.json()
   await connectMongoDB()
   try {
     const cookieStore = cookies()
-    const token = cookieStore.get('token')?.value
+    const token = cookieStore.get('jwtToken')?.value
     const verifiedToken = await verify(token)
 
     if (!verifiedToken) {
-      return res.status(401).json({ message: 'Unauthorized' })
+      return new Response(
+        JSON.stringify({ message: 'Token not found in cookies' }),
+        {
+          status: 401,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
     }
 
-    const data = await req.json()
-
     if (!data?.name) {
-      return res.status(400).json({ message: 'Role name is required' })
+      return new Response(
+        JSON.stringify({ message: 'Role name is required' }),
+        {
+          status: 400,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
     }
 
     const newRole = await Role.create({
@@ -26,19 +41,108 @@ export default async function POST(req, res) {
       // permissions: data.permissions
     })
 
-    return res.status(201).json({
-      message: 'Role added successfully',
-      role: newRole,
-    })
+    return new Response(
+      JSON.stringify({ message: 'Role added successfully' }),
+      {
+        status: 201,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        role: newRole,
+      }
+    )
   } catch (error) {
     if (error instanceof ZodError) {
-      return res.status(400).json({
-        message: 'Validation error',
-        errors: error.errors,
+      return new Response(JSON.stringify({ message: 'Validation error' }), {
+        status: 400,
+        headers: {
+          'Content-Type': 'application/json',
+        },
       })
     } else {
       console.error('Role creation error:', error)
-      return res.status(500).json({ message: 'Internal server error' })
+      return new Response(
+        JSON.stringify({ message: 'Internal Server Error' }),
+        {
+          status: 500,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+    }
+  }
+}
+
+export async function GET() {
+  await connectMongoDB()
+  try {
+    const foundRoles = await Role.find()
+    return new Response(
+      JSON.stringify({ message: 'Roles Found Successfully' }),
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        roles: foundRoles,
+      }
+    )
+  } catch {
+    if (!foundRoles) {
+      return new Response(JSON.stringify({ message: 'Roles Not Found' }), {
+        status: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+    } else {
+      return new Response(
+        JSON.stringify({ message: 'Internal Server Error' }),
+        {
+          status: 500,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+    }
+  }
+}
+
+export async function DELETE(id) {
+  await connectMongoDB()
+  const roleId = new ObjectId(id)
+  const role = await Role.findByIdAndDelete(roleId)
+  try {
+    return new Response(
+      JSON.stringify({ message: 'Role is Deleted Successfully' }),
+      {
+        status: 201,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        role: role,
+      }
+    )
+  } catch {
+    if (!role) {
+      return new Response(JSON.stringify({ message: 'Role id Not Found' }), {
+        status: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+    } else {
+      return new Response(
+        JSON.stringify({ message: 'Internal Server Error' }),
+        {
+          status: 500,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
     }
   }
 }
