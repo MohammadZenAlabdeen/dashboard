bee550558d6f0e87a8ea98053f4586ccc159e4b8
ghaith alqattan <1vvsqm@gmail.com>
2025-04-08 15:24:57 +0300

    roles check update

--- app/api/roles/route.js
+++ app/api/roles/route.js
@@ -1,1 +1,44 @@
-export default async function POST(request, response) {}
+import Role from '@/models/Role'
+import { cookies } from 'next/headers'
+import { ZodError } from 'zod'
+import verify from '@/utils/validate_token'
+import connectMongoDB from '@/utils/mongodb'
+
+export default async function POST(req, res) {
+  await connectMongoDB()
+  try {
+    const cookieStore = cookies()
+    const token = cookieStore.get('token')?.value
+    const verifiedToken = await verify(token)
+
+    if (!verifiedToken) {
+      return res.status(401).json({ message: 'Unauthorized' })
+    }
+
+    const data = await req.json()
+
+    if (!data?.name) {
+      return res.status(400).json({ message: 'Role name is required' })
+    }
+
+    const newRole = await Role.create({
+      name: data.name,
+      // permissions: data.permissions
+    })
+
+    return res.status(201).json({
+      message: 'Role added successfully',
+      role: newRole,
+    })
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json({
+        message: 'Validation error',
+        errors: error.errors,
+      })
+    } else {
+      console.error('Role creation error:', error)
+      return res.status(500).json({ message: 'Internal server error' })
+    }
+  }
+}
--- utils/validation.js
+++ utils/validation.js
@@ -1,16 +1,34 @@
-import { z } from "zod";
+import { z } from 'zod'
 
-export const RegisterSchema = z.object({
-    name: z.string({ message: "name must be a string" }).min(4, "name must be at least 4 letters long"),
-    email: z.string({ message: "email must be a string" }).email("must be a valid mail"),
-    password: z.string({ message: "password must be a string" }).min(8, "passowrd must be at least 8 characters long"),
-    confirmPassword: z.string({ message: "password confirmation must be a string" }),
-}).refine(data => data.password === data.confirmPassword, {
-    message: "Passwords must match",
-    path: ["confirmPassword"],
-});
+export const RegisterSchema = z
+  .object({
+    name: z
+      .string({ message: 'name must be a string' })
+      .min(4, 'name must be at least 4 letters long'),
+    email: z
+      .string({ message: 'email must be a string' })
+      .email('must be a valid mail'),
+    password: z
+      .string({ message: 'password must be a string' })
+      .min(8, 'passowrd must be at least 8 characters long'),
+    confirmPassword: z.string({
+      message: 'password confirmation must be a string',
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords must match',
+    path: ['confirmPassword'],
+  })
 
 export const LoignSchema = z.object({
-    email: z.string({ message: "email must be a string" }).email("must be a valid mail"),
-    password: z.string({ message: "password must be a string" }).min(8, "password must be at least 8 characters long")
+  email: z
+    .string({ message: 'email must be a string' })
+    .email('must be a valid mail'),
+  password: z
+    .string({ message: 'password must be a string' })
+    .min(8, 'password must be at least 8 characters long'),
+})
+
+export const RoleSchema = z.object({
+  role: z.string({ message: 'Role must be string' }),
 })
