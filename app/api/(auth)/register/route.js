d7bab11f1f0995c910d48063bacccdf368f57f08
MohammadZenAlabdeen <myn1ytc@gmail.com>
2025-04-08 14:22:39 +0300

    register

--- app/api/(auth)/register/route.js
+++ app/api/(auth)/register/route.js
@@ -1,6 +1,35 @@
+import User from "@/models/User";
+import generatetoken from "@/utils/generate_token";
+import { hashUserPassword } from "@/utils/hash";
 import { RegisterSchema } from "@/utils/validation";
+import { ZodError } from "zod";
+import { serialize } from "cookie";
 
-export default async function Post(req,res){
+export async function POST(req,res){
     const data=req.body;
+    try{
     RegisterSchema.parse({name:data.name,email:data.email,password:data.password,confirmPassword:data.confirmPassword})
-}
+    const passowrd=hashUserPassword(data.password);
+    const token=generatetoken(data.name,"user");
+    User.create({
+        name:data.name,
+        email:data.email,
+        password:passowrd,
+        token:token,
+    })
+
+    const cookie=serialize({
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', 
+        maxAge: 60 * 60 * 24,
+        path: '/',  
+    })
+    res.setHeader('Set-Cookie', cookie);  
+    return res.status(201).json({ message: 'User registered successfully' });  
+}catch(error){
+    if (error instanceof ZodError) {  
+        return res.status(400).json({ errors: error.errors });  
+    } else {  
+       return res.status(500).json({ message: 'Internal server error' });  
+    }   
+}}
--- utils/generate_token.js
+++ utils/generate_token.js
@@ -1,8 +1,12 @@
 import jwt from "jsonwebtoken"
 import {JwtPayload} from "jsonwebtoken"
 
-export default function generatetoken(){
-    const privateKey=process.env.JSON_SECRET_KEY.toString();
-    const token=jwt.sign(JwtPayload,privateKey,{expiresIn:'30d'});
-    return token;
-}
+export default function generatetoken(name, role) {  
+    const privateKey = process.env.JSON_SECRET_KEY;  
+    const payload = {  
+        name,  
+        role,  
+    };  
+    const token = jwt.sign(payload, privateKey, { expiresIn: '30d' });  
+    return token;  
+} 
