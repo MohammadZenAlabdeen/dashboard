23906a885a63e8782c4fd37a7c93e38748999143
MohammadZenAlabdeen <myn1ytc@gmail.com>
2025-04-08 13:57:08 +0300

    test

--- app/api/(auth)/register/route.js
+++ app/api/(auth)/register/route.js
@@ -1,3 +1,6 @@
+import { RegisterSchema } from "@/utils/validation";
+
 export default async function Post(req,res){
-    
+    const data=req.body;
+    RegisterSchema.parse({name:data.name,email:data.email,password:data.password,confirmPassword:data.confirmPassword})
 }
