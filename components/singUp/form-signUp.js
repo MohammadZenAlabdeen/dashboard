b3c8762547f7eb3d0969a2085cf67bf63410a5a6
omarwalix <omarwali992@gmail.com>
2025-04-13 17:33:15 +0300

    Sign-up preparation

--- components/singUp/form-signUp.js
+++ components/singUp/form-signUp.js
@@ -1,8 +1,44 @@
-import SecondaryIntroBtn from "../all-btn/intro-btn/secondary-intro-btn"
+'use client'
+import { useState } from 'react';
+import SecondaryIntroBtn from '../all-btn/intro-btn/secondary-intro-btn';
+import { ToastContainer, toast } from 'react-toastify';
 
 function SignUpForm() {
+    //  const notifySuccess = () => toast.success(`Sign up Completed ${newUser.name}`);
+    // const notifyFail = () => toast.error('Sorry,Sign Up Failed!');
+    // notifySuccess();
+    // notifyFail();
+
+    const [newUser, setNewUser] = useState({ name: '', email: '', password: '', confirmPassword: '' });
+
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+
+        const apiUrl = 'http://localhost:3000/api/register';
+
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: newUser,
+        })
+            .then((data) => {
+                console.log('Sign Up Done', data)
+
+            })
+            .catch((error) => {
+                console.log('Error Sign Up:', error)
+
+            }
+            );
+    }
+
+
     return (
-        <form className="w-full px-2">
+        <form className="w-full px-2 " onSubmit={handleSubmit}>
             <div className="mb-6 ">
 
                 <h2 className="text-light bg-primary inline text-center px-3 pb-2 rounded min-w-sm text-4xl font-bold ">Sign Up</h2>
@@ -11,23 +47,24 @@ function SignUpForm() {
             </div>
             <div className="mb-5">
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-title-black dark:text-white">Email</label>
-                <input type="email" id="email" className="bg-light border border-secondary text-title-black font-bold text-sm rounded-sm  focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your Email" required />
+                <input type="email" id="email" name="email" value={newUser.email} onChange={(e) => setNewUser({ ...newUser, email: e.target.value })} className="bg-light border border-secondary text-title-black font-bold text-sm rounded-sm  focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your Email" required />
             </div>
             <div className="mb-5">
                 <label htmlFor="name" className="block mb-2 text-sm font-medium text-title-black dark:text-white">Name</label>
-                <input type="name" id="name" className="bg-light border border-secondary text-title-black font-bold text-sm rounded-sm  focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your Name" required />
+                <input type="text" id="name" name="name" value={newUser.name} onChange={(e) => setNewUser({ ...newUser, name: e.target.value })} className="bg-light border border-secondary text-title-black font-bold text-sm rounded-sm  focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your Name" required />
             </div>
             <div className="mb-5">
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-title-black dark:text-white">Password</label>
-                <input type="password" id="password" className="bg-light border border-secondary text-title-black font-bold text-sm rounded-sm  focus:border-light block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="********" required />
+                <input type="password" id="password" name="password" value={newUser.password} onChange={(e) => setNewUser({ ...newUser, password: e.target.value })} className="bg-light border border-secondary text-title-black font-bold text-sm rounded-sm  focus:border-light block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="********" required />
             </div>
             <div className="mb-5">
                 <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-title-black dark:text-white">Confirm confirmPassword</label>
-                <input type="confirmPassword" id="confirmPassword" className="bg-light border border-secondary text-title-black font-bold text-sm rounded-sm  focus:border-light block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="********" required />
+                <input type="password" id="confirmPassword" value={newUser.confirmPassword} onChange={(e) => setNewUser({ ...newUser, confirmPassword: e.target.value })} className="bg-light border border-secondary text-title-black font-bold text-sm rounded-sm  focus:border-light block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="********" required />
             </div>
-            <SecondaryIntroBtn href='/'>
+            <SecondaryIntroBtn >
                 Sign Up
             </SecondaryIntroBtn>
+            <ToastContainer autoClose={3000} />
         </form>
     )
 }
